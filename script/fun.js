fea080c91800a137a0cd8221139ed32829cec06e
jferreira94 <41697523+jferreira94@users.noreply.github.com>
2019-11-24 23:30:59 -0300

    molde do formulário

--- script/fun.js
+++ script/fun.js
@@ -1,3 +1,22 @@
+firebase.auth().currentUser;
+
+function login(){
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+}
+
+
+
 function submeter(form){
 
   if(form.grupo.value == "invalido")
